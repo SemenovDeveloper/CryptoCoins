2adba9a8c392094e077bd0f153671d4d877abe82
SemenovDev <semenovdev@yahoo.com>
2025-05-12 14:04:59 +0600

    view binding

--- app/src/main/java/com/semenovdev/cryptocoins/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/semenovdev/cryptocoins/presentation/CoinPriceListActivity.kt
@@ -3,17 +3,21 @@ package com.semenovdev.cryptocoins.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.semenovdev.cryptocoins.R
+import com.semenovdev.cryptocoins.databinding.ActivityCoinPriceListBinding
 import com.semenovdev.cryptocoins.domain.CoinInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
+    private val binding by lazy {
+        ActivityCoinPriceListBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_prce_list)
+        setContentView(binding.root)
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinInfo: CoinInfo) {
@@ -24,8 +28,8 @@ class CoinPriceListActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
-        rvCoinPriceList.adapter = adapter
+
+        binding.rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.priceList.observe(this) {
             adapter.coinInfoList = it
