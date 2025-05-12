a3bd2c30ae7e7cb7e48c183441c3136c30db9902
SemenovDev <semenovdev@yahoo.com>
2025-05-12 14:23:27 +0600

    adapter view binding

---
+++ app/src/main/java/com/semenovdev/cryptocoins/presentation/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.semenovdev.cryptocoins.presentation
+
+import androidx.recyclerview.widget.RecyclerView
+import com.semenovdev.cryptocoins.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
